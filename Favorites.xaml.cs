651f4b95ed8c1ea878232793698849db4d5945e9
Blagovest Pizhev <pizhevsoft@gmail.com>
2021-05-13 14:22:28 +0300

    Add files via upload

---
+++ Favorites.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace WeatherLocationInfo.Views
+{
+    public partial class Favorites : ContentPage
+    {
+        ObservableCollection<string> CityData { get; set; }
+
+        public Favorites()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            CityData = new ObservableCollection<string>();
+            this.BindingContext = this;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        public void AddCity(string city)
+        {
+            CityData.Add(city);
+        }
+
+    }
+}
